83b748204fca13d2e4a90a42b6e47af6c1261956
David Hernández Cárdenas <davidher28@javerianacali.edu.co>
2018-11-17 12:31:20 -0500

    Delete _primary.vhd

--- simulation/qsim/work/@f@l@i@p_@f@l@o@p/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity FLIP_FLOP is
-    port(
-        D               : in     vl_logic;
-        CLK             : in     vl_logic;
-        Q               : out    vl_logic
-    );
-end FLIP_FLOP;
